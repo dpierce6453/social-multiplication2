d3946e79fbc97a870caeb6d33e4b98581378a67b
Dan Pierce <dan@pierceclan.net>
2023-03-07 15:59:42 -0500

    Starting changes for event bus

---
+++ src/main/java/microservices/book/multiplication/event/MultiplicationSolvedEvent.java
@@ -0,0 +1,52 @@
+package microservices.book.multiplication.event;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class MultiplicationSolvedEvent implements Serializable {
+    private final Long multiplicationResultAttemptId;
+    private final Long playerId;
+    private final boolean correct;
+
+    public MultiplicationSolvedEvent(Long multiplicationResultAttemptId, Long playerId, boolean correct) {
+        this.multiplicationResultAttemptId = multiplicationResultAttemptId;
+        this.playerId = playerId;
+        this.correct = correct;
+    }
+
+    public Long getMultiplicationResultAttemptId() {
+        return multiplicationResultAttemptId;
+    }
+
+    public Long getPlayerId() {
+        return playerId;
+    }
+
+    public boolean isCorrect() {
+        return correct;
+    }
+
+    @Override
+    public String toString() {
+        return "MultiplicationSolvedEvent{" +
+                "multiplicationResultAttemptId=" + multiplicationResultAttemptId +
+                ", playerId=" + playerId +
+                ", correct=" + correct +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MultiplicationSolvedEvent that = (MultiplicationSolvedEvent) o;
+        return isCorrect() == that.isCorrect() &&
+                getMultiplicationResultAttemptId().equals(that.getMultiplicationResultAttemptId())
+                && getPlayerId().equals(that.getPlayerId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getMultiplicationResultAttemptId(), getPlayerId(), isCorrect());
+    }
+}
