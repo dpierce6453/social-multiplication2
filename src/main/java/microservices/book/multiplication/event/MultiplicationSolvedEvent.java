35d59e8ddff712ab70b12b4904a6c64d59b92d1d
dan.pierce <dan@pierceclan.net>
2023-11-26 16:43:39 -0500

    Using Lombok in MultiplicationSolvedEvent

--- src/main/java/microservices/book/multiplication/event/MultiplicationSolvedEvent.java
+++ src/main/java/microservices/book/multiplication/event/MultiplicationSolvedEvent.java
@@ -1,50 +1,20 @@
 package microservices.book.multiplication.event;
 
-import java.io.Serializable;
-import java.util.Objects;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
 public class MultiplicationSolvedEvent implements Serializable {
+
     private final Long multiplicationResultAttemptId;
     private final Long playerId;
     private final boolean correct;
-
-    public MultiplicationSolvedEvent(Long multiplicationResultAttemptId, Long playerId, boolean correct) {
-        this.multiplicationResultAttemptId = multiplicationResultAttemptId;
-        this.playerId = playerId;
-        this.correct = correct;
-    }
-
-    public Long getMultiplicationResultAttemptId() {
-        return multiplicationResultAttemptId;
-    }
-
-    public Long getPlayerId() {
-        return playerId;
-    }
-
-    public boolean isCorrect() {
-        return correct;
-    }
-
-    @Override
-    public String toString() {
-        return "MultiplicationSolvedEvent{" +
-                "multiplicationResultAttemptId=" + multiplicationResultAttemptId +
-                ", playerId=" + playerId +
-                ", correct=" + correct +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MultiplicationSolvedEvent that = (MultiplicationSolvedEvent) o;
-        return correct == that.correct && Objects.equals(multiplicationResultAttemptId, that.multiplicationResultAttemptId) && Objects.equals(playerId, that.playerId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(multiplicationResultAttemptId, playerId, correct);
-    }
 }
